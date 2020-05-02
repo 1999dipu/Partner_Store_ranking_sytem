549430555749519af4e05caa01c081c8dc358c4b
nainagupta49 <nainaguptaorange@gmail.com>
2020-05-03 00:49:40 +0530

    changes in algo.py

--- final-traversy/leadmanager/algo.py
+++ final-traversy/leadmanager/algo.py
@@ -1,11 +1,42 @@
 import sqlite3
 
-# Create a SQL connection to our SQLite database
 con = sqlite3.connect("db.sqlite3")
+
 cur = con.cursor()
 
-for row in cur.execute('SELECT lockers_onboard.name,lockers_onboard.lockerid,lockers_availability.lockerid,lockers_availability.non_del_days FROM lockers_onboard ,lockers_availability WHERE  lockers_onboard.lockerid =lockers_availability.lockerid;'):
-    print(row)
+cur.execute('delete from locker_rankinglist')
+qry="""select locker_availability.lockerid_id,
+        locker_onboard.name,
+        locker_onboard.country,
+        locker_onboard.address,
+        locker_onboard.zipcode,
+        locker_availability.non_del_days,
+        locker_availability.timings_open,
+        locker_availability.timings_closed,
+        locker_availability.status 
+        from locker_onboard,locker_availability 
+        where 
+        locker_availability.lockerid_id = locker_onboard.lockerid;"""
+qryInsrt = """insert into locker_rankinglist 
+            (lockerid_id,
+            name,
+            country,
+            address,
+            zipcode,
+            non_del_days,
+            timings_open,
+            timings_closed,
+            status,
+            rank) values (?,?,?,?,?,?,?,?,?,?);"""
+rank=1
+lstRow=[]
+for row in cur.execute(qry):
+    row+=(rank,)
+    rank=rank+1
+    lstRow.append(row)
+    
+cur.executemany(qryInsrt,lstRow)
 
-# Be sure to close the connection
+for row in cur.execute('select * from locker_rankinglist;'):
+    print(row)
 con.close()
