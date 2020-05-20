5b65e053a8d57de5c2dc1e4d4d05109176caea3f
nainagupta49 <nainaguptaorange@gmail.com>
2020-05-20 15:54:03 +0530

    python specs added

--- final-traversy/leadmanager/algorithm/algo.py
+++ final-traversy/leadmanager/algorithm/algo.py
@@ -1,8 +1,19 @@
 from algorithm import getRows
+from algorithm import getSpecs
 from algorithm import updRankingList
 
 def algoFun(qry):
     x=qry["lattitude"]
     y=qry["longitude"]
-    rows=getRows.getrows(x,y)
-    updRankingList.updateR(rows)
+    lockers=getRows.getrows(x,y)
+    specLocker=[]
+    for row in lockers:
+        x=()
+        x+=(row[0],)
+        x=getSpecs.specs(row[0])
+        x+=(row[9],)
+        specLocker.append(x)
+    print(specLocker)
+    for y in specLocker:
+        y+=("Naina",)
+    updRankingList.updateR(lockers)
