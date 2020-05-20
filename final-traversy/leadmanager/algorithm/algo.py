f84d5ba05e7428d1bc6e909af2f83107eaba998d
nainagupta49 <nainaguptaorange@gmail.com>
2020-05-20 14:59:53 +0530

    updateRankingList.py added

--- final-traversy/leadmanager/algorithm/algo.py
+++ final-traversy/leadmanager/algorithm/algo.py
@@ -1,26 +1,8 @@
-import sqlite3
 from algorithm import getRows
+from algorithm import updRankingList
 
 def algoFun(qry):
     x=qry["lattitude"]
     y=qry["longitude"]
     rows=getRows.getrows(x,y)
-    con = sqlite3.connect("db.sqlite3")
-    cur = con.cursor()
-    cur.execute('delete from locker_rankinglist')
-    qryInsrt = """insert into locker_rankinglist 
-                (lockerid_id,
-                name,
-                country,
-                address,
-                zipcode,
-                non_del_days,
-                timings_open,
-                timings_closed,
-                status,
-                dist,
-                rank) values (?,?,?,?,?,?,?,?,?,?,?);"""
-    cur.executemany(qryInsrt,rows)
-    con.commit()
-    print("RANKING DINE")
-    con.close()
+    updRankingList.updateR(rows)
